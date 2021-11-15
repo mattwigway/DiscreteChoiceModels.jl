7cfbf56906dcf4d88844b1fcd3e905186c3b401d
Matt Bhagat-Conway <matt@indicatrix.org>
2021-11-15 17:17:15 -0500

    export \beta macro

--- src/DiscreteChoiceModels.jl
+++ src/DiscreteChoiceModels.jl
@@ -1,11 +1,13 @@
 module DiscreteChoiceModels
 
+using OnlineStats, Tables, JuliaDB, JuliaDBMeta, Printf, Statistics, LinearAlgebra
+
 include("LogitModel.jl")
 include("util.jl")
 include("utilitymacro.jl")
 include("mnl.jl")
 include("compute_utility.jl")
 
-export multinomial_logit, @utility
+export multinomial_logit, @utility, @β
 
 end
