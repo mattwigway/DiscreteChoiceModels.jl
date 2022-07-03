879b0dfccfe389e73cd73c09026a34d1e23383f0
Matt Bhagat-Conway <matt@indicatrix.org>
2022-07-03 11:07:30 -0400

    exponentiate rather than squaring non-negative coefficients

--- mixed.jl
+++ mixed.jl
@@ -10,10 +10,10 @@ function main(data)
             1 ~ -βtt * tt1 + -βtc * tc1 + -βhw * hw1 + -βch * ch1
             2 ~ -βtt * tt2 + -βtc * tc2 + -βhw * hw2 + -βch * ch2
 
-            βtt = LogNormal(-3, 0.0001^2), level=>ID
-            βtc = LogNormal(-3, 0.0001^2), level=>ID
-            βhw = LogNormal(-3, 0.0001^2), level=>ID
-            βch = LogNormal(-3, 0.0001^2), level=>ID
+            βtt = LogNormal(-3, exp(-4.6052)), level=>ID
+            βtc = LogNormal(-3, exp(-4.6052)), level=>ID
+            βhw = LogNormal(-3, exp(-4.6052)), level=>ID
+            βch = LogNormal(-3, exp(-4.6052)), level=>ID
             # βtt = LogNormal(-1.9834, exp(-0.7631405523804803)), level=>ID
             # βtc = LogNormal(-1.0246, exp(-0.01582)), level=>ID
             # βhw = LogNormal(-2.9339, exp(-0.2030)), level=>ID
