6c6fbb407afa0b8f187f3a2266323f69da23ddbd
Matt Bhagat-Conway <matt@indicatrix.org>
2023-11-21 10:41:01 -0500

    fix comment

--- test/mnl/infinite_exputil.jl
+++ test/mnl/infinite_exputil.jl
@@ -1,6 +1,6 @@
 # In the original, naive implementation of the multinomial logit likelihood, we just calculated exp(util) / sum(exp.(utils))
 # but this can lead to overflow when util is large. This didn't seem to affect convergence, somehow, but is
-# still undesirable. Now we use the softmax function from LogExpFunctions which handles overflow.
+# still undesirable. Now we use the logsumexp function from LogExpFunctions which avoids overflow.
 
 @testitem "Infinite exp(util)" begin
     import LogExpFunctions: softmax
